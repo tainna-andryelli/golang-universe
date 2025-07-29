ba94db9271d2f22f6a3f5eecacf053189d8413bf
tainna-andryelli <tainnaandryelli@gmail.com>
2025-07-28 21:02:09 -0300

    fix: remove unused import in main

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/tainna-andryelli/golang-universe/codewars"
 )
 
 func main() {
-	fmt.Println(codewars.ArrayDiff([]int{1, 2, 3}, []int{2}))
+	fmt.Println("Welcome to the Golang Universe!")
 }
